dad724ab60a6879798fe3a3fb0cb49a7b3092267
mishagrol <grol81@mail.ru>
2020-04-18 22:43:48 +0300

    gav

--- Python_basics/main.py
+++ Python_basics/main.py
@@ -4,12 +4,22 @@ class Dog():
         """Инициализируем атрибуты имя и возраст"""
         self.name = name
         self.age = age
-        print("Собака создана")
+        #print("Собака создана")
     def sit(self):
         """Собака будет садиться по команде"""
-        print(self.name.title() + "собака села")
+        print(self.name.title() + " собака села")
 
     def jump(self):
         """Собака будет прыгать по команде"""
-        print(self.name.title() + "собака прыгнула")
+        print(self.name.title() + " собака прыгнула")
         
+my_dog = Dog('Topik', 4)
+my_dog_2 = Dog('Nick', 2)
+
+print(my_dog.age)
+print(my_dog.name)
+
+my_dog.sit()
+my_dog_2.jump()
+
+poppy = Dog('Poppy', 5)
